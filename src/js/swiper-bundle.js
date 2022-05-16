a56c21f1ed3081f5bd7f572521a7d4a567d07f8d
D-Pavlenko <denispavlenko94@gmail.com>
2022-05-16 19:56:20 +0300

    ElectricCar

---
+++ src/js/swiper-bundle.js
@@ -0,0 +1,36 @@
+'.slider-img', {
+
+    loop: false,
+    speed: 2400,
+    parallax: true,
+
+    navigation: {
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next'
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+    },
+
+    mousewheel: {
+        invert: false
+    },
+
+    mousewheel: {
+        sensitivity: 1,
+    },
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+    },
+}
